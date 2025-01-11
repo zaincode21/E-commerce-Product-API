d095fdbc0f77de407ab634d8dc9c36f9a4f07079
zaincode21 <sergedukuziyaremye.com>
2025-01-11 10:25:31 +0200

    this code is coded by serge for finaly project

---
+++ ecommerce_api/products/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Product, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'category', 'stock_quantity', 'image_url']
+
+    def create(self, validated_data):
+        category_data = validated_data.pop('category')
+        category, created = Category.objects.get_or_create(**category_data)
+        product = Product.objects.create(category=category, **validated_data)
+        return product
---
+++ ecommerce_api/products/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
